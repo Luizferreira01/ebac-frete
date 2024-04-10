9726bc38511806d417cfa6b76899ca108026f6c1
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-04-09 21:44:51 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('#cep').mask('00000-000');
+
+    $('#btn-buscar-cep').click(function() {
+        const cep = $('#cep').val();
+        const endpoint = `https:viacep.com.br/ws/${cep}/json`;
+        const botao = $(this);
+        $(botao).find('i').addClass('d-none');
+
+        $.ajax(endpoint).done(function(resposta) {
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $('#endereco').val(endereco)
+
+            setTimeout(function() {
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 4000);
+        })
+    })
+})
